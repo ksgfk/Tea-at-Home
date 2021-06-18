a1014831c16681bac75709c6cfa4c1a919b34420
2501741939 <38825526+2501741939@users.noreply.github.com>
2021-06-18 16:13:16 +0800

    添加 产品的增删查改

---
+++ Tea-at-Home/src/com/github/ksgfk/teaathome/servlet/QueryProductServlet.java
@@ -0,0 +1,66 @@
+package com.github.ksgfk.teaathome.servlet;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.math.BigDecimal;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.github.ksgfk.teaathome.control.impl.ControlProduct;
+import com.github.ksgfk.teaathome.control.inter.ControlProductInter;
+import com.github.ksgfk.teaathome.models.Product;
+import com.github.ksgfk.teaathome.utility.JsonUtility;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.stream.JsonWriter;
+
+/**
+ * Servlet implementation class QueryProductServlet
+ */
+@WebServlet("/product/fuzzy_query")
+public class QueryProductServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    private ControlProductInter productInter=null;
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public QueryProductServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+        productInter = new ControlProduct();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 JsonWriter jsonWriter= new JsonWriter(new OutputStreamWriter(response.getOutputStream()));
+			JsonElement userData = JsonUtility.read(request);
+	        JsonObject root = userData.getAsJsonObject();
+	        String name = root.get("key").getAsString();
+	        List<Product> list=productInter.findname(name);
+	        if(list==null&&list.size()==0) {
+	        	response.setStatus(404);
+	        	jsonWriter.flush();
+	   	        jsonWriter.close();
+	        	return;
+	        }
+	        JsonUtility.toJson(list, list.getClass(), jsonWriter);
+	        jsonWriter.flush();
+	        jsonWriter.close();
+	}
+
+}
---
+++ Tea-at-Home/src/com/github/ksgfk/teaathome/servlet/UpdataProductSevlet.java
@@ -0,0 +1,85 @@
+package com.github.ksgfk.teaathome.servlet;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.math.BigDecimal;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.github.ksgfk.teaathome.control.impl.ControlProduct;
+import com.github.ksgfk.teaathome.control.inter.ControlProductInter;
+import com.github.ksgfk.teaathome.models.Product;
+import com.github.ksgfk.teaathome.utility.JsonUtility;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.stream.JsonWriter;
+
+/**
+ * Servlet implementation class UpdataProductSevlet
+ */
+@WebServlet("/product/fuzzy_updata")
+public class UpdataProductSevlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    private ControlProductInter productInter=null;
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UpdataProductSevlet() {
+        super();
+        // TODO Auto-generated constructor stub
+        productInter = new ControlProduct();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 	JsonWriter jsonWriter= new JsonWriter(new OutputStreamWriter(response.getOutputStream()));
+			JsonElement userData = JsonUtility.read(request);
+	        JsonObject root = userData.getAsJsonObject();
+	        String name = root.get("name").getAsString();
+	        int  count= root.get("count").getAsInt(); 
+			int product_id=root.get("count").getAsInt();
+			int depository_id=root.get("productid").getAsInt();
+			BigDecimal price =root.get("price").getAsBigDecimal();
+			if(!productInter.updata(new Product(depository_id, name, count, depository_id, price))) {
+				writeRegisterFailed(jsonWriter, "修改错误");
+			}
+			else{
+				writeRegisterSuccess(jsonWriter);
+			};
+			   jsonWriter.flush();
+		        jsonWriter.close();
+		
+	}
+    public static void writeRegisterFailed(JsonWriter writer, String message) throws IOException {
+        writer.beginObject();
+        writer.name("success");
+        writer.value(false);
+        writer.name("message");
+        writer.value(message);
+        writer.endObject();
+    }
+
+    public static void writeRegisterSuccess(JsonWriter writer) throws IOException {
+        writer.beginObject();
+        writer.name("success");
+        writer.value(true);
+        writer.name("message");
+        writer.nullValue();
+        writer.endObject();
+    }
+
+}
